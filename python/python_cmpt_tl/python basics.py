6fcc271ee2222c61aba3576226ccb0f160235be5
Anand27git <anandkumar256k@gmail.com>
2026-01-08 23:24:20 +0530

    operators in python

--- python/python_cmpt_tl/python basics.py
+++ python/python_cmpt_tl/python basics.py
@@ -124,3 +124,39 @@ elif temperature > 20:
 else:
     print("It's cold")
 print("Done")
+
+age = 22
+
+if age >= 18:
+    message = "Eligible"
+else:
+    message = "Not Eligible"
+    print(message)
+# using ternary operator above expression return same
+message = "Eligible" if age >= 18 else "Not Eligible"
+print (message)
+
+
+# Logical operators
+#and
+#or
+#not
+high_income = True
+good_credit = True
+student = False
+
+if high_income and good_credit: # both condition should be true
+    print("Eligible"")
+else:
+    print("Not Eligible")
+    
+    
+if not student:                     
+    print("Eligible"")
+else:
+    print("Not Eligible")
+    
+    
+    
+    
+    
